af81f052c877f347c9b02dd5d2264b429a8e752c
Zack Galbreath <zack.galbreath@kitware.com>
2020-06-23 16:56:20 -0400

    Update entrypoint to use new python implementation

--- gh-gl-sync/entrypoint.sh
+++ gh-gl-sync/entrypoint.sh
@@ -1,8 +1,7 @@
-sh /scripts/install-packages.sh
 export GITLAB_SSH_KEY_BASE64="$( base64 < /secrets/gitlab-ssh-key )"
 export GITHUB_TOKEN="$( cat /secrets/github-access-token )"
 # TODO: update these repos after testing
-bash /scripts/github-prs-to-gitlab.sh \
+python3 /scripts/SpackCIBridge.py \
   'scottwittenburg/gitlab-ci-tests' \
   'ssh.gitlab.spack.io' \
-  'scott/gitlab-ci-tests'
+  'scott/gitlab-ci-tests' \
