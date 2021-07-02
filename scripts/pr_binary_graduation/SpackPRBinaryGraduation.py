0635c0b00c04c5b4a3d53d8d68432c949eaeddbf
Scott Wittenburg <scott.wittenburg@kitware.com>
2021-07-02 11:57:14 -0600

    Add webserver to handle webhooks from github

--- scripts/pr_binary_graduation/SpackPRBinaryGraduation.py
+++ scripts/pr_binary_graduation/SpackPRBinaryGraduation.py
@@ -2,11 +2,17 @@
 
 import argparse
 import asyncio
+import hmac
+
+import aiohttp.web as aiohttp_web
 
 import llnl.util.tty as tty
 import spack.main as spack_main
 import spack.util.url as url_util
 import spack.util.web as web_util
+
+
+PR_MIRROR_BASE_URL = 's3://spack-binaries-prs'
 
 
 async def run_in_subprocess(cmd_string):
@@ -53,6 +59,27 @@ async def test_methods(src_mirror, dest_mirror):
     await update_mirror_index(dest_mirror)
 
 
+async def webhook_handler(request):
+    if request.content_type != 'application/json':
+        print('Ignoring non-JSON request')
+        return
+
+    payload = await request.json()
+
+    if payload['action'] == 'closed' and payload['pull_request']['merged'] == True:
+        pr_number = payload['number']
+        pr_branch = payload['pull_request']['head']['ref']
+
+        pr_mirror_url = '{0}/github/pr{1}_{2}'.format(PR_MIRROR_BASE_URL, pr_number, pr_branch)
+        shared_mirror_url = '{0}/shared_pr_mirror'.format(PR_MIRROR_BASE_URL)
+
+        print('this is a merged PR')
+        print('Copying binaries from {0} to {1}'.format(pr_mirror_url, shared_mirror_url))
+
+        # await copy_mirror_entries(pr_mirror_url, shared_mirror_url)
+        # await update_mirror_index(shared_mirror_url)
+
+
 if __name__ == "__main__":
     # Parse command-line arguments.
     parser = argparse.ArgumentParser(description="Start PR binary graduation webservice")
@@ -61,7 +88,11 @@ if __name__ == "__main__":
     print('Spack version:')
     print(spack_main.get_version())
 
-    src = 's3://spack-binaries-prs/github/pr24637_pipelines-add-broken-spec-details'
-    dest = 's3://spack-binaries-prs/shared-pr-mirror/test'
+    # src = 's3://spack-binaries-prs/github/pr24637_pipelines-add-broken-spec-details'
+    # dest = 's3://spack-binaries-prs/shared-pr-mirror/test'
 
-    asyncio.run(test_methods(src, dest))
+    # asyncio.run(test_methods(src, dest))
+
+    app = aiohttp_web.Application()
+    app.add_routes([aiohttp_web.post('/payload', webhook_handler)])
+    aiohttp_web.run_app(app, port=4567)
