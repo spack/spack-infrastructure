652d669e5271c6e8677464cc2f5a99deb05447f7
Chris Kotfila <chris.kotfila@kitware.com>
2022-05-12 16:35:53 -0400

    Update release and sign.sh script

--- images/notary/sign.sh
+++ images/notary/sign.sh
@@ -1,30 +1,52 @@
 #!/usr/bin/env bash
 
 # Pre-flight
-[ "$#" -lt 1 ] && { echo >&2 "No arguments. Aborting."; exit 1; }
+
+# TODO uncomment when ready to take inputs
+# [ "$#" -lt 1 ] && { echo >&2 "No arguments. Aborting."; exit 1; }
 command -v gpg >/dev/null 2>&1 || { echo >&2 "Command 'gpg' not found.  Aborting."; exit 1; }
 command -v aws-encryption-cli >/dev/null 2>&1 || { echo >&2 "Command 'aws-encryption-cli' not found.  Aborting."; exit 1; }
 
-# TODO Set this to wherever we mount the tmpfs filesystem
-WORKINGDIR=$(pwd)
+WORKINGDIR=/mnt/gnupg
 
-ENCRYPTED_SUBKEY=subkey.encrypted.gpg
 KMS_KEY_ARN=arn:aws:kms:us-east-1:588562868276:key/e811e4c5-ea63-4da3-87d4-664dc5395169
 
-# Generate temporary directory
-GNUGPDIR=$(mktemp -d -t XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -p $WORKINGDIR)
-trap "rm -rf $GNUGPDIR" EXIT
+SIGNING_KEY=/mnt/keys/signing/signing_key.encrypted.gpg
+SIGNING_KEY_ID=CA0118FBB810A56C120BA6DF5D70BDA102F772BA
 
-# Decrypt private signing key
-gpg --homedir $GNUGPDIR \
-    --no-options \
-    --require-secmem \
-    --quiet \
-    --import <( aws-encryption-cli --decrypt -S \
-                -w key=${KMS_KEY_ARN}\
-                -i ${ENCRYPTED_SUBKEY} -o -)
+INTERMEDIATE_CI_KEY=/mnt/keys/signing/intermediate_ci_public_key.gpg
+INTERMEDIATE_CI_KEY_ID=78F3726939CA1B94893B66E8BC86F6FB94429164
+
+
+# Generate temporary directory
+# TODO  this template doesn't work,  only last 8 are random ?
+export GNUPGHOME=$(mktemp -d -t XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -p $WORKINGDIR)
+
+# TODO Uncomment when ready to deploy
+# trap "rm -rf $GNUGPDIR" EXIT
+
+# Decrypt private signing key, import and trust
+# 78F3726939CA1B94893B66E8BC86F6FB94429164
+gpg --import <( aws-encryption-cli --decrypt -S \
+                -w "key=${KMS_KEY_ARN}" \
+                -i ${SIGNING_KEY} -o -)
+echo -e "5\ny\n" | gpg --command-fd 0 --edit-key "$SIGNING_KEY_ID" trust
+
+
+# Import public intermediate key and trust
+gpg --import $INTERMEDIATE_CI_KEY
+echo -e "5\ny\n" | gpg --command-fd 0 --edit-key "$INTERMEDIATE_CI_KEY_ID" trust
+
+
+
+# TODO Check downloaded spec files,  die if not signed/verified
+
+# TODO Find all intermediate signed files and strip/resign
+
+
+
 
 # Sign files
-for FILE; do
-    [ -f "${FILE}" ] && gpg --homedir "${GNUGPDIR}" --output "${FILE}.sig" --detach-sig "${FILE}"
-done
+# for FILE; do
+#    [ -f "${FILE}" ] && gpg --homedir "${GNUGPDIR}" --output "${FILE}.sig" --detach-sig "${FILE}"
+# done
