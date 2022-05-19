eb6ae078f0fea4b4a00cf042e50d4844acc1a49a
Chris Kotfila <chris.kotfila@kitware.com>
2022-05-19 12:48:57 -0400

    Handle tty issues with sign.sh in CI

--- images/notary/sign.sh
+++ images/notary/sign.sh
@@ -3,10 +3,21 @@ set -Eeuo pipefail
 
 # Pre-flight
 
-# TODO uncomment when ready to take inputs
-# [ "$#" -lt 1 ] && { echo >&2 "No arguments. Aborting."; exit 1; }
 command -v gpg >/dev/null 2>&1 || { echo >&2 "Command 'gpg' not found.  Aborting."; exit 1; }
 command -v aws-encryption-cli >/dev/null 2>&1 || { echo >&2 "Command 'aws-encryption-cli' not found.  Aborting."; exit 1; }
+
+
+# Remove the access key and secret key environment variables that come in from
+# the CI pipeline. This script relies on permissions confired to the pod via the
+# service account assigned to it. That means it requires an AWS_ROLE_ARN and
+# AWS_WEB_IDENTITY_TOKEN_FILE variables from EKS. Because of credential
+# discovery precidence, boto (used by aws-encryption-cli) will find the
+# AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY first if they are not unset.
+#
+# See Also:
+# https://docs.aws.amazon.com/eks/latest/userguide/iam-roles-for-service-accounts-technical-overview.html#pod-configuration
+unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+
 
 WORKINGDIR=${WORKINGDIR:-/tmp}
 mkdir -p $WORKINGDIR
@@ -27,41 +38,34 @@ GNUPGHOME=/mnt/gnupg/$(echo $RANDOM | md5sum | head -c 20)
 mkdir -p -m 0700 $GNUPGHOME
 export GNUPGHOME
 
-# TODO Uncomment when ready to deploy
+# Remove keyring on exit
 trap "rm -rf $GNUPGHOME" EXIT
 
 
+# Import public keys
+gpg --no-tty --import $INTERMEDIATE_CI_PUBLIC_KEY
+gpg --no-tty --import $SIGNING_PUBLIC_KEY
+
+# Trust public keys
+gpg --import-ownertrust <(echo -e "${INTERMEDIATE_CI_PUBLIC_KEY_ID}:6:\n${SIGNING_KEY_ID}:6:")
 
 
-# Import public intermediate key and trust
-gpg --import $INTERMEDIATE_CI_PUBLIC_KEY
-echo -e "5\ny\n" | gpg --command-fd 0 --edit-key "$INTERMEDIATE_CI_PUBLIC_KEY_ID" trust
-
-
-# Import public reputational key and trust
-gpg --import $SIGNING_PUBLIC_KEY
-echo -e "5\ny\n" | gpg --command-fd 0 --edit-key "$SIGNING_KEY_ID" trust
-
-# TODO Check downloaded spec files,  die if not signed/verified
-
-
-
+# Check downloaded spec files,  die if not signed/verified
 for FILE in $WORKINGDIR/*; do
     echo "VERIFY: ${FILE}"
-    gpg --quiet ${FILE}
+    gpg --no-tty --quiet ${FILE}
     rm ${FILE}
 done
 
 
-# Decrypt private signing key, and import
-# 78F3726939CA1B94893B66E8BC86F6FB94429164
-gpg --import <( aws-encryption-cli --decrypt -S \
-                -w "key=${KMS_KEY_ARN}" \
-                -i ${SIGNING_KEY} -o -)
+# Import the private key for signing
+gpg --no-tty --import <(aws-encryption-cli --decrypt -S -w "key=${KMS_KEY_ARN}" -i ${SIGNING_KEY} -o -)
 
 
+# Sign Keys with reputational key
 for FILE in $WORKINGDIR/*; do
    echo "SIGN: ${FILE}"
-   gpg --output ${FILE}.sig --clearsign ${FILE}
+   gpg --no-tty --output ${FILE}.sig --clearsign ${FILE}
    rm ${FILE}
 done
+
