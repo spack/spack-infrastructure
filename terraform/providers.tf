20528d407da966f7342f106dfbf651124e38351f
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-01-10 14:04:05 -0500

    Configure s3 backend for terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.3.6"
 
+  backend "s3" {
+    bucket         = "spack-infrastructure-terraform"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
