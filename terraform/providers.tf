09c7fffc2f1cfbff2d4a1b7714090bcd27114241
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-01-19 12:20:26 -0500

    Fix kube aws-auth error

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -39,40 +39,38 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role", aws_iam_role.eks_cluster_access.arn]
-    }
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.default.token
   }
+}
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_name
+
+  depends_on = [
+    module.eks.cluster_name
+  ]
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_name
+
+  depends_on = [
+    module.eks.cluster_name
+  ]
 }
 
 provider "kubectl" {
-  apply_retry_count      = 5
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role", aws_iam_role.eks_cluster_access.arn]
-  }
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role", aws_iam_role.eks_cluster_access.arn]
-  }
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 
