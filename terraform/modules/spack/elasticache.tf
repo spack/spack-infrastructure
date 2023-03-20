7f09ce620b8ad8c0616fedb9f06d0b20df2607f6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-03-20 11:15:58 -0400

    Manage elasticache engine/version with Terraform

--- terraform/modules/spack/elasticache.tf
+++ terraform/modules/spack/elasticache.tf
@@ -7,6 +7,8 @@ resource "aws_elasticache_replication_group" "pr_binary_graduation_task_queue" {
   replication_group_id = "pr-binary-graduation-queue-${var.deployment_name}"
   description          = "Used by python RQ module to store pending tasks for workers"
 
+  engine               = "redis"
+  engine_version       = "7.0"
   node_type            = "cache.t3.small"
   port                 = 6379
   parameter_group_name = "default.redis7"
