a7b6e4c931699388752611f347122a9148d2c555
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-04-11 14:33:15 -0400

    Run `terraform fmt`

--- terraform/modules/spack/vpc.tf
+++ terraform/modules/spack/vpc.tf
@@ -6,17 +6,17 @@ module "vpc" {
   name = "spack-${var.deployment_name}"
   cidr = var.vpc_cidr
 
-  azs = var.availability_zones
-  public_subnets = var.public_subnets
-  private_subnets = var.private_subnets
+  azs              = var.availability_zones
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
   database_subnets = var.database_subnets
 
   # Create a DB subnet group for RDS (see rds.tf)
   create_database_subnet_group = true
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = false
-  enable_dns_hostnames = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  enable_dns_hostnames   = true
   one_nat_gateway_per_az = true
 
   public_subnet_tags = {
