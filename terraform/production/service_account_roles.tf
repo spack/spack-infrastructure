d395cb2241449697f3e8c9ee3666a534da6cc7ea
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-05-12 08:58:07 -0400

    Encode spackbot role/service account into TF

--- terraform/production/service_account_roles.tf
+++ terraform/production/service_account_roles.tf
@@ -75,3 +75,74 @@ resource "kubectl_manifest" "notary_service_account" {
     aws_iam_role_policy.notary
   ]
 }
+
+# The IAM role granting Spackbot full access to spack-binaries-prs S3 bucket.
+resource "aws_iam_role" "full_crud_access_spack_binaries_prs" {
+  name        = "FullCRUDAccessToBucketSpackBinariesPRs"
+  description = "Managed by Terraform. Grants Kubernetes pods access to read/write/delete objects from the spack-binaries-prs S3 bucket."
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : module.production_cluster.oidc_provider_arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${module.production_cluster.oidc_provider}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "put_spack_binaries_prs" {
+  name        = "PutObjectsInBucketSpackBinariesPRs"
+  description = "Managed by Terraform. Grants permission to PutObject for any object in the spack-binaries-prs bucket."
+  role        = aws_iam_role.full_crud_access_spack_binaries_prs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::spack-binaries-prs/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "delete_spack_binaries_prs" {
+  name        = "DeleteObjectsFromBucketSpackBinariesPRs	"
+  description = "Managed by Terraform. Grants permission to DeleteObject for any object in the spack-binaries-prs bucket."
+  role        = aws_iam_role.full_crud_access_spack_binaries_prs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "s3:DeleteObject",
+        "Resource" : "arn:aws:s3:::spack-binaries-prs/*"
+      }
+    ]
+  })
+}
+
+resource "kubectl_manifest" "spackbot_service_account" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: ServiceAccount
+    metadata:
+      name: spackbot-spack-io
+      namespace: spack
+      annotations:
+        eks.amazonaws.com/role-arn: ${aws_iam_role.full_crud_access_spack_binaries_prs.arn}
+  YAML
+  depends_on = [
+    aws_iam_role_policy.put_spack_binaries_prs,
+    aws_iam_role_policy.delete_spack_binaries_prs
+  ]
+}
