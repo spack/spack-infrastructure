eec2f617e095e83a868140f7d951f70a0384967d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-07-21 15:30:01 -0400

    Add IAM role/policy for GitLab access to buckets

--- terraform/modules/spack/gitlab_object_stores.tf
+++ terraform/modules/spack/gitlab_object_stores.tf
@@ -28,3 +28,62 @@ resource "aws_s3_bucket_policy" "gitlab_object_stores" {
     ]
   })
 }
+
+resource "aws_iam_policy" "gitlab_object_stores" {
+  name        = "GitlabS3Role-${var.deployment_name}"
+  description = "Managed by Terraform. Grants required permissions for GitLab to read/write to relevant S3 buckets."
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          # https://docs.gitlab.com/ee/install/aws/manual_install_aws.html#create-an-iam-policy
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl",
+        ],
+        "Resource" : [for bucket in aws_s3_bucket.gitlab_object_stores : "${bucket.arn}/*"],
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket",
+          "s3:AbortMultipartUpload",
+          "s3:ListMultipartUploadParts",
+          "s3:ListBucketMultipartUploads"
+        ],
+        "Resource" : [for bucket in aws_s3_bucket.gitlab_object_stores : bucket.arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "gitlab_object_stores" {
+  name        = "GitlabS3Role-${var.deployment_name}"
+  description = "Managed by Terraform. Role for GitLab to assume so that it can access relevant S3 buckets."
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : module.eks.oidc_provider_arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${module.eks.oidc_provider}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "gitlab_object_stores" {
+  role       = aws_iam_role.gitlab_object_stores.name
+  policy_arn = aws_iam_policy.gitlab_object_stores.arn
+}
