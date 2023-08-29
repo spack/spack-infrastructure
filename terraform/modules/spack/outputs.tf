ee5be90f4cb9f21f730c2870dca7acf6c1d422da
Jacob Nesbitt <jjnesbitt2@gmail.com>
2023-08-29 14:23:30 -0400

    Add cluster_arn output to spack module

--- terraform/modules/spack/outputs.tf
+++ terraform/modules/spack/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "cluster_arn" {
+  value = module.eks.cluster_arn
+}
+
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
