ba913fa1978c9561b78df508b9de291004d95825
Jacob Nesbitt <jjnesbitt2@gmail.com>
2023-08-29 17:32:06 -0400

    Allow any branch to authorize to AWS using the GithubActions role

--- terraform/production/github_actions_iam.tf
+++ terraform/production/github_actions_iam.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "github_actions" {
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
           "StringLike" : {
-            "token.actions.githubusercontent.com:sub" : "repo:spack/spack-infrastructure:ref:refs/heads/main",
+            "token.actions.githubusercontent.com:sub" : "repo:spack/spack-infrastructure:ref:refs/heads/*",
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           }
         }
