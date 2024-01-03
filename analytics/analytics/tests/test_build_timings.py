3f7f1c97a80d1525c7d68102ff1e9660c8ca82e2
Dan LaManna <dan.lamanna@kitware.com>
2024-01-03 12:21:59 -0500

    Add bare bones testing infrastructure

---
+++ analytics/analytics/tests/test_build_timings.py
@@ -0,0 +1,29 @@
+import json
+import pytest
+from analytics.job_log_uploader import upload_job_log
+from analytics.build_timing_processor import upload_build_timings
+
+from analytics.models import Timer
+
+
+@pytest.fixture()
+def build_json_string():
+    return json.dumps(
+        {
+            "project_id": 2,
+            "build_id": 9708962,
+            "project": {"web_url": "https://aspackgitlaburl.test"},
+        }
+    )
+
+
+@pytest.mark.django_db
+def test_upload_build_timings(build_json_string):
+    upload_build_timings(build_json_string)
+
+    assert Timer.objects.exists()
+
+
+@pytest.mark.django_db
+def test_upload_job_logs(build_json_string):
+    upload_job_log(build_json_string)
