47439e412a303f4ac44126cb68a7230df7c2beda
Ryan Krattiger <ryan.krattiger@kitware.com>
2024-04-04 11:20:17 -0500

    Added some more logging and error suppression in non-critical paths

--- images/ci-prune-buildcache/entrypoint.sh
+++ images/ci-prune-buildcache/entrypoint.sh
@@ -7,22 +7,24 @@ now=${1:-$(date --iso-8601)}
 
 snapshot_dir="$PWD/snapshot-${now}"
 
-# Use the $SPACKROOT/etc/ dir for configs
-export SPACK_DISABLE_LOCAL_CONFIG=1
-
-# Get spack
+# Configure spack
 if [ ! -d spack ]; then
   git clone --depth 1 --branch develop https://github.com/spack/spack.git
 fi
+. ./spack/share/spack/setup-env.sh
+
+# Use the $SPACKROOT/etc/ dir for configs
+export SPACK_DISABLE_LOCAL_CONFIG=1
 
 function add_mirrors() {
   # Add the mirrors
-  cat ${snapshot_dir}/config | jq '.stacks | keys[] as $k | "./spack/bin/spack mirror add \($k) \(.[$k])"' | sed 's/"//g' | bash
+  # Don't fail if they are already there... || true may be too heavy haned but I don't think there is any other reason for this command to fail if the
+  cat ${snapshot_dir}/config | jq '.stacks | keys[] as $k | "echo Adding mirror for \($k) \(.[$k]) && spack mirror add \($k) \(.[$k]) || true"' | sed 's/"//g' | bash
 }
 
 function update_index() {
   # Update the mirror indices
-  cat ${snapshot_dir}/config | jq '.stacks | keys[] as $k | "./spack/bin/spack buildcache update-index \($k)"' | sed 's/"//g' | bash
+  cat ${snapshot_dir}/config | jq '.stacks | keys[] as $k | "echo Updating index for \($k); spack buildcache update-index \($k)"' | sed 's/"//g' | bash
 }
 
 # Perform direct pruning
