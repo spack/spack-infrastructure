744ae55e389f29e876f2490f01eb8dd87f206e2e
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-06-20 18:02:18 -0400

    WIP

--- secrets/src/spack_secrets/update.py
+++ secrets/src/spack_secrets/update.py
@@ -9,12 +9,23 @@ import click
 import kubernetes.config
 from kubernetes.client import CoreV1Api
 
-from .curses import select_secret_and_key
+from .curses import select_secret, select_secret_key
 from .sealed import decrypt_sealed_secret, get_kubeseal_path, seal_raw_secret_value, seal_secret
 from .utils import exit_prompt, get_yaml_reader, populated_tempfile
 
 if typing.TYPE_CHECKING:
     from kubernetes.client.models.v1_config_map import V1ConfigMap
+
+
+NEW_SECRET_TEMPLATE = """# Please modify the secret to your liking here. The data field will be automatically encoded to
+# base64 once saved, so you may just input the desired values as plain text.
+apiVersion: v1
+kind: Secret
+metadata:
+  name: mysecret
+  namespace: mynamespace
+data:
+  foo: bar"""  # noqa: S105
 
 
 class InvalidClusterInfoError(Exception):
@@ -65,22 +76,18 @@ def handle_raw_secret_input():
     click.echo(encrypted_value)
 
 
-def handle_secret_update(secrets_file: str, value: str):
-    secrets_file_path = Path(secrets_file)
-    if not secrets_file_path.exists():
-        raise click.ClickException(f"File {secrets_file} not found")
-    if secrets_file_path.is_dir():
-        raise click.ClickException("Argument SECRETS_FILE must be a file, not a folder.")
+def handle_create_new_secret():
+    value = read_user_input(starting_value=NEW_SECRET_TEMPLATE.encode()).strip()
+    value = "\n".join([line for line in value.splitlines() if not line.startswith("#")])
+    secret = get_yaml_reader().load(value)
+    for key, value in secret["data"].items():
+        secret["data"][key] = base64.b64encode(value.encode("utf-8")).decode("utf-8")
 
-    # Read in supplied secret file
-    yl = get_yaml_reader()
-    with open(secrets_file) as f:
-        secret_docs = list(yl.load_all(f))
+    return secret
 
-    # Retrieve the secret and key to update
-    sealed_secret, secret_index, key_to_update, adding_new_key = select_secret_and_key(
-        secret_docs=secret_docs
-    )
+
+def handle_secret_update(sealed_secret: dict, value: str):
+    key_to_update, adding_new_key = select_secret_key(secret=sealed_secret)
     secret = decrypt_sealed_secret(sealed_secret)
 
     # Retrieve value, if not already supplied
@@ -112,16 +119,43 @@ def handle_secret_update(secrets_file: str, value: str):
     # Update existing secret with new value
     secret["data"][key_to_update] = base64.b64encode(value.encode("utf-8")).decode("utf-8")
 
+    return secret
+
+
+def handle_file_update(secrets_file: str, value: str):
+    secrets_file_path = Path(secrets_file)
+    if not secrets_file_path.exists():
+        raise click.ClickException(f"File {secrets_file} not found")
+    if secrets_file_path.is_dir():
+        raise click.ClickException("Argument SECRETS_FILE must be a file, not a folder.")
+
+    # Read in supplied secret file
+    yl = get_yaml_reader()
+    with open(secrets_file) as f:
+        secret_docs: list[dict] = list(yl.load_all(f))
+
+    # Get selected sealed secret
+    sealed_secret, secret_index, new_secret = select_secret(secret_docs=secret_docs)
+    secret = (
+        handle_create_new_secret()
+        if new_secret
+        else handle_secret_update(sealed_secret=sealed_secret, value=value)
+    )
+
     # Encrypt value using kubeseal
     resealed_secret = seal_secret(secret)
+    if new_secret:
+        secret_docs.append(resealed_secret)
+    else:
+        # Update all the values of the original sealed_secret "spec.encryptedData" field,
+        # to preserve comments, etc.
+        for k, v in resealed_secret["spec"]["encryptedData"].items():
+            sealed_secret["spec"]["encryptedData"][k] = v
 
-    # Update all the values of the original sealed_secret "spec.encryptedData" field,
-    # to preserve comments, etc.
-    for k, v in resealed_secret["spec"]["encryptedData"].items():
-        sealed_secret["spec"]["encryptedData"][k] = v
+        # Update secret dict and save
+        secret_docs[secret_index] = sealed_secret
 
-    # Update secret dict and save
-    secret_docs[secret_index] = sealed_secret
+    # Write out
     with open(secrets_file, "w") as f:
         yl.dump_all(secret_docs, f)
 
@@ -138,11 +172,21 @@ def handle_secret_update(secrets_file: str, value: str):
     help="Returns an encrypted value directly, instead of updating a file",
 )
 @click.option(
+    "--secret",
+    type=click.STRING,
+    help="The name of the secret to update, in the format <namespace>.<name>",
+)
+@click.option(
+    "--key",
+    type=click.STRING,
+    help="The key within the secret data field to update",
+)
+@click.option(
     "--value",
     type=click.STRING,
     help="Supply the value for the selected secret as an argument.",
 )
-def update(secrets_file: str, *, value: str, raw: bool):
+def update(secrets_file: str, *, secret: str, key: str, value: str, raw: bool):
     if secrets_file is None and not raw:
         raise click.ClickException(
             "Argument SECRETS_FILE must be supplied when --raw is not specified"
@@ -164,4 +208,4 @@ def update(secrets_file: str, *, value: str, raw: bool):
         handle_raw_secret_input()
         return
 
-    handle_secret_update(secrets_file=secrets_file, value=value)
+    handle_file_update(secrets_file=secrets_file, value=value)
