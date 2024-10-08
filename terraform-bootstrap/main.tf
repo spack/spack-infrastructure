7c75e0744435116b29cba49496143a007e17e36d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-10-08 12:05:09 -0400

    Consolidate TF state into 1 bucket/dynamodb table

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.4.6"
+  required_version = "~> 1.9.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -48,9 +48,10 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "spack-terraform-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  name                        = "spack-terraform-state-locks"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "LockID"
+  deletion_protection_enabled = true
 
   attribute {
     name = "LockID"
