f6c245433c066f799c0483fdad1492353ae415f4
Stephen Sachs <stesachs@amazon.com>
2024-10-18 14:12:58 +0200

    Enable stesachs@amazon.com to run `s3 sync` on a sub-bucket
    I want to add to the existing buildcache location and run `spack buildcache
    update-index` in order to merge the content. Spack needs to be able to list the
    content (or use `aws s3 sync`) to run `buildcache update-index`.
    
    This is an extension of #730 to enable `aws s3 sync` additionally to `aws s3 cp`.

--- terraform/modules/spack_aws_k8s/bootstrap_s3.tf
+++ terraform/modules/spack_aws_k8s/bootstrap_s3.tf
@@ -36,9 +36,12 @@ resource "aws_s3_bucket_policy" "bootstrap" {
           "AWS" : "arn:aws:iam::679174810898:root"
         },
         "Action" : [
+          "s3:CopyObject*",
+          "s3:DeleteObject*",
           "s3:GetObject*",
-          "s3:PutObject*",
-          "s3:DeleteObject*"
+          "s3:ListBucket*"
+          "s3:ListObjects*",
+          "s3:PutObject*"
         ],
         "Resource" : "arn:aws:s3:::${aws_s3_bucket.bootstrap.bucket}/pcluster/*"
       }
