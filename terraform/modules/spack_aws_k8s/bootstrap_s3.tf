bd0c3eedf4ddd984c29bad527f9d85fd50e72dd2
Stephen Sachs <stesachs@amazon.com>
2024-10-21 14:43:30 +0200

    Add ListBucket permission to enable `aws s3 sync`
    Remove non-existent s3 actions
    
    `s3:CopyObjects` and `s3:ListObjects` are not registered as IAM s3 actions. Fixes #977

--- terraform/modules/spack_aws_k8s/bootstrap_s3.tf
+++ terraform/modules/spack_aws_k8s/bootstrap_s3.tf
@@ -36,9 +36,10 @@ resource "aws_s3_bucket_policy" "bootstrap" {
           "AWS" : "arn:aws:iam::679174810898:root"
         },
         "Action" : [
+          "s3:DeleteObject*",
           "s3:GetObject*",
-          "s3:PutObject*",
-          "s3:DeleteObject*"
+          "s3:ListBucket*",
+          "s3:PutObject*"
         ],
         "Resource" : "arn:aws:s3:::${aws_s3_bucket.bootstrap.bucket}/pcluster/*"
       }
