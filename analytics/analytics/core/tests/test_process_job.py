397944ad6a1ea8230596f17512d8b486350459fb
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-11-01 13:38:24 -0400

    [WIP] Enable testing suite

---
+++ analytics/analytics/core/tests/test_process_job.py
@@ -0,0 +1,15 @@
+import pathlib
+
+import pytest
+
+from analytics.job_processor import process_job
+
+examples = pathlib.Path(__file__).parent / "data" / "example_payloads"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("payload_path", examples.iterdir())
+def test_process_job_known_payloads(payload_path: pathlib.Path):
+    with open(payload_path) as f:
+        data = f.read()
+        process_job(data)
