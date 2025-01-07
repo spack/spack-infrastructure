d03d1d330df62cb8e148d7ee1888d4dc9b8d0fcb
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-01-06 22:24:45 -0500

    Fix classifier for Django
    We're running Django 5.1 now.

--- analytics/setup.py
+++ analytics/setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
-        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.1",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
