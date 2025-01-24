7ab99e7ca90a9b3a442e242daa34b2be047fb5e2
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-01-23 20:13:15 -0500

    Switch to `ci_builds_metadata.exit_code` for getting exit code

--- analytics/analytics/job_processor/dimensions.py
+++ analytics/analytics/job_processor/dimensions.py
@@ -17,7 +17,7 @@ from analytics.core.models.dimensions import (
     TimeDimension,
 )
 from analytics.job_processor.metadata import JobMiscInfo, NodeInfo, PackageInfo, PodInfo
-from analytics.job_processor.utils import get_job_retry_data
+from analytics.job_processor.utils import get_job_exit_code, get_job_retry_data
 
 UNNECESSARY_JOB_REGEX = re.compile(r"No need to rebuild [^,]+, found hash match")
 BUILD_STAGE_REGEX = r"^stage-\d+$"
@@ -141,11 +141,7 @@ def create_job_data_dimension(
     runner_version = rvmatch.group(1) if rvmatch is not None else ""
     unnecessary = UNNECESSARY_JOB_REGEX.search(job_trace) is not None
 
-    # The last line is an ANSI escape sequence, so we need to get the second to last line
-    # to get the actual last line of the job trace
-    job_trace_last_line = job_trace.splitlines()[-2]
-    exit_code_match = re.search(r"exit code (\d+)", job_trace_last_line)
-    job_exit_code = int(exit_code_match.group(1)) if exit_code_match is not None else None
+    job_exit_code = get_job_exit_code(job_id=job_id)
 
     job_data = JobDataDimension.objects.create(
         job_id=job_id,
