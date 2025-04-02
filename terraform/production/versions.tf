289dc84a4ef7110b6a7372020fa2462eb9826765
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-04-02 17:56:18 -0400

    TF: switch to S3 native state locking
    The Terraform S3 backend now supports locking with only S3, no longer
    requiring a DynamoDB table. See
    https://developer.hashicorp.com/terraform/language/backend/s3#state-locking

--- terraform/production/versions.tf
+++ terraform/production/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = "~> 1.11.3"
 
   backend "s3" {
-    bucket         = "spack-terraform-state"
-    key            = "production/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "spack-terraform-state-locks"
-    encrypt        = true
+    bucket       = "spack-terraform-state"
+    key          = "production/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
