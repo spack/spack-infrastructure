847b8f6f919027d0b483f3f0d1094b3f3c34639b
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-04-02 17:58:03 -0400

    Deprovision terraform state dynamodb table
    This is no longer needed now that we are using native S3 state
    locking.

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -46,15 +46,3 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name                        = "spack-terraform-state-locks"
-  billing_mode                = "PAY_PER_REQUEST"
-  hash_key                    = "LockID"
-  deletion_protection_enabled = true
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
