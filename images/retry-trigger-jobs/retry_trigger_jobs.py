9d00424d4c747a858854d762b9a525bef2e5ee9b
Zack Galbreath <zack.galbreath@kitware.com>
2025-07-02 09:03:39 -0400

    Point retry-trigger-jobs mitigation at spack-packages repo

--- images/retry-trigger-jobs/retry_trigger_jobs.py
+++ images/retry-trigger-jobs/retry_trigger_jobs.py
@@ -11,7 +11,7 @@ import sentry_sdk
 
 sentry_sdk.init(traces_sample_rate=0.01)
 
-GITLAB_API_URL = "https://gitlab.spack.io/api/v4/projects/2"
+GITLAB_API_URL = "https://gitlab.spack.io/api/v4/projects/"
 
 
 def paginate(session, query_url):
@@ -42,7 +42,7 @@ def print_response(resp, padding=''):
     print(f"{padding}response value: {resp.text}")
 
 
-def retry_trigger_jobs(last_n_hours):
+def retry_trigger_jobs(last_n_hours, projectid):
     """Analyze pipelines updated over the last_n_hours to find and retry
     child pipelines whose generate jobs failed initially but succeeded
     upon retry."""
@@ -66,13 +66,13 @@ def retry_trigger_jobs(last_n_hours):
     # Iterate over recent pipelines.
     dt = datetime.now(timezone.utc) - timedelta(hours=last_n_hours)
     time_threshold = urllib.parse.quote_plus(dt.isoformat(timespec="seconds"))
-    pipelines_url = f"{GITLAB_API_URL}/pipelines?updated_after={time_threshold}"
+    pipelines_url = f"{GITLAB_API_URL}/{projectid}/pipelines?updated_after={time_threshold}"
     pipelines = paginate(session, pipelines_url)
     for pipeline in pipelines:
         print(f"Checking pipeline {pipeline['id']}: {pipeline['ref']}")
         # Iterate over the trigger jobs ("bridges") for this parent pipeline.
         parent_id = pipeline['id']
-        bridges_url = f"{GITLAB_API_URL}/pipelines/{parent_id}/bridges"
+        bridges_url = f"{GITLAB_API_URL}/{projectid}/pipelines/{parent_id}/bridges"
         bridges = paginate(session, bridges_url)
         for bridge in bridges:
             if not bridge["downstream_pipeline"]:
@@ -90,7 +90,7 @@ def retry_trigger_jobs(last_n_hours):
                 continue
 
             # 3) The child pipeline does not have any jobs.
-            child_jobs_url = f"{GITLAB_API_URL}/pipelines/{child_id}/jobs"
+            child_jobs_url = f"{GITLAB_API_URL}/{projectid}/pipelines/{child_id}/jobs"
             child_jobs = paginate(session, child_jobs_url)
             if len(child_jobs) != 0:
                 continue
@@ -103,7 +103,7 @@ def retry_trigger_jobs(last_n_hours):
             generate_job_name = bridge["name"].replace("-build", "-generate")
             found_success = False
             found_failed = False
-            parent_jobs_url = f"{GITLAB_API_URL}/pipelines/{parent_id}/jobs?include_retried=true"
+            parent_jobs_url = f"{GITLAB_API_URL}/{projectid}/pipelines/{parent_id}/jobs?include_retried=true"
             parent_jobs = paginate(session, parent_jobs_url)
             for job in parent_jobs:
                 if job["name"] == generate_job_name:
@@ -116,7 +116,7 @@ def retry_trigger_jobs(last_n_hours):
                         # generate job, retry the trigger job to fix the
                         # child pipeline.
                         print(f"!!! Retrying job #{bridge['id']} to fix pipeline {child_id}")
-                        retry_url = f"{GITLAB_API_URL}/jobs/{bridge['id']}/retry"
+                        retry_url = f"{GITLAB_API_URL}/{projectid}/jobs/{bridge['id']}/retry"
                         print_response(session.post(retry_url))
                         break
 
@@ -131,13 +131,21 @@ def main():
         description="Retry child pipelines that failed to generate initially",
     )
     parser.add_argument(
-        "hours",
+        "--hours",
         type=int,
+        required=True,
         help="Number of hours to look back for failed child pipelines"
     )
+    parser.add_argument(
+        "--projectid",
+        type=int,
+        required=True,
+        help="Gitlab project ID to search"
+    )
+
     args = parser.parse_args()
 
-    retry_trigger_jobs(args.hours)
+    retry_trigger_jobs(args.hours, args.projectid)
 
 
 ################################################################################
