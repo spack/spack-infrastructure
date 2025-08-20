9672026f97df64ba49a7a11676130aefba1263d6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-08-20 10:43:21 -0400

    Upgrade Flux and pin other TF dependencies

--- terraform/staging/versions.tf
+++ terraform/staging/versions.tf
@@ -1,6 +1,29 @@
 terraform {
   required_version = "~> 1.11.3"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "2.1.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = "1.6.4"
+    }
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "17.6.1"
+    }
+  }
+
   backend "s3" {
     bucket       = "spack-terraform-state"
     key          = "staging/terraform.tfstate"
