56d93e70ff9164ab7064e788ebc7bf77ea07a453
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-10-30 13:35:55 -0400

    Upgrade to EKS 1.33

--- terraform/modules/spack_aws_k8s/eks.tf
+++ terraform/modules/spack_aws_k8s/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "21.8.0"
 
   name    = local.eks_cluster_name
-  kubernetes_version = "1.32"
+  kubernetes_version = "1.33"
 
   # Give the Terraform identity admin access to the cluster
   # which will allow it to deploy resources into the cluster
