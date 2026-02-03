a0f9aaa28545b13f4f16a040cd350115836a4a87
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-02-03 16:00:02 -0500

    Setup controller helm release and IAM role association

--- terraform/modules/spack_aws_k8s/vpc_lattice.tf
+++ terraform/modules/spack_aws_k8s/vpc_lattice.tf
@@ -61,3 +61,38 @@ resource "aws_iam_policy" "vpc_lattice_controller" {
     ]
   })
 }
+
+# IAM role for the AWS Gateway API Controller for VPC Lattice (EKS Pod Identity)
+resource "aws_iam_role" "vpc_lattice_controller" {
+  name        = "VPCLatticeControllerIAMRole-${var.deployment_name}"
+  description = "IAM Role for AWS Gateway API Controller for VPC Lattice"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEksAuthToAssumeRoleForPodIdentity"
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_lattice_controller" {
+  role       = aws_iam_role.vpc_lattice_controller.name
+  policy_arn = aws_iam_policy.vpc_lattice_controller.arn
+}
+
+resource "aws_eks_pod_identity_association" "vpc_lattice_controller" {
+  cluster_name    = module.eks.cluster_name
+  role_arn        = aws_iam_role.vpc_lattice_controller.arn
+  namespace       = "aws-application-networking-system"
+  service_account = "gateway-api-controller"
+}
