d385d5f2bc33fa91eace001b113c5908e8822901
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-02-11 14:32:42 -0500

    WIP

--- terraform/modules/spack_aws_k8s/vpc_lattice.tf
+++ terraform/modules/spack_aws_k8s/vpc_lattice.tf
@@ -29,6 +29,19 @@ resource "aws_security_group_rule" "vpc_lattice_ingress_all" {
   ]
   security_group_id = module.eks.cluster_primary_security_group_id
 }
+# TODO: May be needed? Didn't seem to affect anything when I tried it.
+# resource "aws_security_group_rule" "vpc_lattice_ingress_nodes" {
+#   type      = "ingress"
+#   from_port = "-1"
+#   to_port   = "-1"
+#   protocol  = "-1"
+#   prefix_list_ids = [
+#     data.aws_ec2_managed_prefix_list.cluster_prefix_list.id,
+#     data.aws_ec2_managed_prefix_list.cluster_prefix_list_ipv6.id
+#   ]
+#   security_group_id = module.eks.node_security_group_id
+#   description       = "Allow VPC Lattice traffic to reach pods on worker nodes"
+# }
 
 # VPC Lattice controller IAM policy
 resource "aws_iam_policy" "vpc_lattice_controller" {
@@ -120,4 +133,100 @@ resource "aws_eks_pod_identity_association" "vpc_lattice_controller" {
   role_arn        = aws_iam_role.vpc_lattice_controller.arn
   namespace       = "aws-application-networking-system"
   service_account = "gateway-api-controller"
+}
+
+resource "kubectl_manifest" "external_dns_namespace" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: external-dns
+  YAML
+}
+
+#
+# External DNS Helm Release
+resource "helm_release" "external_dns" {
+  namespace        = "external-dns"
+  create_namespace = false
+
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  version    = "1.20.0"
+  values = [
+    <<-EOT
+    serviceAccount:
+      create: true
+    sources:
+      - gateway-httproute
+    env:
+      - name: AWS_DEFAULT_REGION
+        value: ${var.region}
+    EOT
+  ]
+}
+
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "external_dns_pod_identity" {
+  name               = "external-dns-pod-identity"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+resource "aws_iam_policy" "external_dns_access" {
+  name        = "AllowExternalDNSUpdates"
+  description = "Allows ExternalDNS to update Route53"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "route53:ChangeResourceRecordSets",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResources"
+        ],
+        "Resource" : [
+          "arn:aws:route53:::hostedzone/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "route53:ListHostedZones"
+        ],
+        "Resource" : [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns_route53" {
+  role       = aws_iam_role.external_dns_pod_identity.name
+  policy_arn = aws_iam_policy.external_dns_access.arn
+}
+
+# EKS Pod Identity for External DNS operator
+# connects Service Account and EDO Namespace (even if not created yet)
+resource "aws_eks_pod_identity_association" "external_dns_pod_identity" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "external-dns"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.external_dns_pod_identity.arn
 }
