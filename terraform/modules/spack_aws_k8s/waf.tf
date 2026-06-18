0a8f359a9983e888755487391d248d81436b18a9
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-06-18 13:54:36 -0400

    Add WAF for spack cluster gateway

---
+++ terraform/modules/spack_aws_k8s/waf.tf
@@ -0,0 +1,212 @@
+resource "aws_wafv2_ip_set" "vpc_nat_ips" {
+  name               = "vpc-nat-ips${local.suffix}"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = [for ip in module.vpc.nat_public_ips : "${ip}/32"]
+}
+
+resource "aws_wafv2_web_acl" "gateway" {
+  name        = "spack-gateway-waf${local.suffix}"
+  scope       = "REGIONAL"
+  description = "WAF protection for the spack Gateway ALB"
+
+  default_action {
+    allow {}
+  }
+
+  # Allow requests originating from inside the cluster VPC
+  rule {
+    name     = "AllowVpcNatIps"
+    priority = 0
+
+    action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.vpc_nat_ips.arn
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AllowVpcNatIps"
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesAmazonIpReputationList"
+
+        rule_action_override {
+          name = "AWSManagedIPReputationList"
+          action_to_use {
+            block {}
+          }
+        }
+
+        rule_action_override {
+          name = "AWSManagedReconnaissanceList"
+          action_to_use {
+            block {}
+          }
+        }
+
+        rule_action_override {
+          name = "AWSManagedIPDDoSList"
+          action_to_use {
+            count {}
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesCommonRuleSet"
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesBotControlRuleSet"
+    priority = 4
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesBotControlRuleSet"
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesBotControlRuleSet"
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesAnonymousIpList"
+    priority = 5
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesAnonymousIpList"
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesAnonymousIpList"
+    }
+  }
+
+  # Issue a javascript-based challenge to any remaining requests
+  rule {
+    name     = "gitlab-challenge"
+    priority = 6
+
+    action {
+      challenge {}
+    }
+
+    statement {
+      byte_match_statement {
+        search_string = "gitlab.${local.domain_suffix}spack.io"
+        field_to_match {
+          single_header {
+            name = "host"
+          }
+        }
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
+        positional_constraint = "EXACTLY"
+      }
+    }
+
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "gitlab-challenge"
+    }
+  }
+
+  visibility_config {
+    sampled_requests_enabled   = true
+    cloudwatch_metrics_enabled = true
+    metric_name                = "spack-gateway-waf${local.suffix}"
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "gateway" {
+  resource_arn = data.aws_lb.gateway.arn
+  web_acl_arn  = aws_wafv2_web_acl.gateway.arn
+}
