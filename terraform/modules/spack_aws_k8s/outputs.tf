7fe59c0ac3d7b05bce0c9397b38197038de80a07
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-08-26 14:03:20 -0400

    Add S3 bucket outputs to spack_aws_k8s module

--- terraform/modules/spack_aws_k8s/outputs.tf
+++ terraform/modules/spack_aws_k8s/outputs.tf
@@ -1,3 +1,11 @@
 output "nat_public_ips" {
   value = module.vpc.nat_public_ips
 }
+
+output "pr_binary_mirror_bucket_name" {
+  value = module.pr_binary_mirror.bucket_name
+}
+
+output "protected_binary_mirror_bucket_name" {
+  value = module.protected_binary_mirror.bucket_name
+}
